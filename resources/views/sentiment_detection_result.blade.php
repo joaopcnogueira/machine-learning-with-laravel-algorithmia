af99346ebe1b46a3d7f496722a8c0c4ffdb7bc28
João Nogueira <joaonogueira@fisica.ufc.br>
2021-06-22 11:44:08 -0300

    prevendo quantidade de caracteres de uma mensagem

--- resources/views/sentiment_detection_result.blade.php
+++ resources/views/sentiment_detection_result.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>SMS Spam Prediction Results</title>
+  <title>Sentiment Detection Results</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link
@@ -18,24 +18,24 @@
 <body>
   <div class="container text-center center-block">
     <h1 class="text-center">PREDICTION RESULTS</h1>
-    <h4>Message Sent:</h4>
+    <h4>Mensagem Enviada:</h4>
     <p>{{ $message }}</p>
     <hr />
-    <h4>Detection Result:</h4>
+    <h4>Sentimento Detectado:</h4>
 
     @if($result == "normal messsage")
-    <p>The message sent is <strong><span style="color: green;">{{ $result }}</span></strong> </p>
+    <p>A mensagem enviada tem <strong><span style="color: green;">{{ $result }}</span></strong> caracteres.</p>
 
     @else
 
-    <p> The message sent is <strong><span style="color: red;">{{ $result }}</span></strong> </p>
+    <p> A mensagem enviada tem <strong><span style="color: red;">{{ $result }}</span></strong> caracteres.</p>
     
     @endif 
     
     <br />
 
     <h3 class="text-center">
-      <a href="/" class="btn btn-lg btn-primary" title="">Try Again</a>
+      <a href="/" class="btn btn-lg btn-primary" title="">Tenta Novamente</a>
     </h3>
   </div>
 
