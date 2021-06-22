71cc5137e7e440367e35d8d9b0a4fdf5b8ae99cc
João Nogueira <joaonogueira@fisica.ufc.br>
2021-06-22 11:36:00 -0300

    template configurado

--- app/Http/Controllers/SentimentController.php
+++ app/Http/Controllers/SentimentController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Algorithmia;
 use Illuminate\Http\Request;
 
 class SentimentController extends Controller
 {
-    //
+    public function show_form()
+    {
+        return view('sentiment_detection_form');
+    }
+
+    public function predict(Request $request) 
+    {
+        // validate the input
+        $this->validate($request, [
+            'message' => 'required|string|min:2'
+        ]);
+
+        // collect the message
+        $message = $request['message'];
+
+        // add client and call the API
+        $api_key = env('ALGORITHMIA_API_KEY');
+        $client = Algorithmia::client($api_key);
+        $model = $client->algo('joaopcnogueira4team/sentiment_score_detection/1.0.0');
+
+        // perform prediction and save result
+        $result = $model->pipe($message)->result;
+
+        return view('sentiment_detection_result', compact(['message', 'result']));
+    }
 }
